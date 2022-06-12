931a12f9b8efcf0d632c461f36c68818ad738a0d
kota-yata <kota@yatagai.com>
2022-06-11 23:45:28 -0500

    Modify svelte.config.js

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -16,7 +16,7 @@ export class WS {
       this.localId = id;
     });
     try {
-      this.ws = new WebSocket('ws://localhost:8080/ws');
+      this.ws = new WebSocket('wss://wsjava.herokuapp.com/ws');
       this.logListStore.pushWithCurrentTimeStamp('Connecting to the signaling server...');
     } catch (err) {
       this.logListStore.pushWithCurrentTimeStamp('Failed to connect to the signaling server');

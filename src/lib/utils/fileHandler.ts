2e1d7ed335f5162632ecca090bf3bbd659c78e37
kota-yata <kota@yatagai.com>
2025-04-19 17:01:49 +0900

    fix #17

--- src/lib/utils/fileHandler.ts
+++ src/lib/utils/fileHandler.ts
@@ -1,20 +1,21 @@
 import { FileObject } from '$lib/objects/fileObject';
 import type { FragmentSet } from '$lib/types';
 
+const MAX_PAYLOAD_SIZE = 16384;
+
 export const fragment = async (file: File, dataId: string): Promise<FragmentSet> => {
-  const THRESHOLD = 16000;
   const fragments: ArrayBuffer[] = [];
-  if (file.size <= THRESHOLD) {
+  if (file.size <= MAX_PAYLOAD_SIZE) {
     const arrayBuffer: ArrayBuffer = await file.arrayBuffer();
     fragments.push(arrayBuffer);
     const fileObject: FileObject = new FileObject(dataId, file.name, file.type, fragments);
     return { fileObject, fragments };
   }
   // Fragment process
-  const total = Math.ceil(file.size / THRESHOLD);
+  const total = Math.ceil(file.size / MAX_PAYLOAD_SIZE);
   for (let i = 1; i <= total; i++) {
-    const start = THRESHOLD * (i - 1);
-    const end = THRESHOLD * i;
+    const start = MAX_PAYLOAD_SIZE * (i - 1);
+    const end = MAX_PAYLOAD_SIZE * i;
     const fragmentBlob = file.slice(start, end, file.type);
     const arrayBuffer: ArrayBuffer = await fragmentBlob.arrayBuffer();
     fragments.push(arrayBuffer);
